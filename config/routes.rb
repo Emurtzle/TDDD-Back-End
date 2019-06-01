5756415b26ebba9f7b6010ed194a8d628bee178c
Emily Wagner <EmilyWagner598@gmail.com>
2019-06-01 15:45:52 -0700

    for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,19 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  namespace :api do
-    namespace :v1 do
       
-      resources :users, only: [:create]
-      
-      resources :clients do
-        resources :duedates
-      end
-
-      post '/login', to: 'auth#create'
-      get '/profile', to: 'users#profile'
-
-      post '/clients/csv', to: 'clients#csv'
-
-      get '/duedates', to: 'duedates#getAll'
-      post '/duedates/createMany', to: 'duedates#createMany'
-      
+    resources :users, only: [:create]
+    
+    resources :clients do
+      resources :duedates
     end
-  end
+
+    post '/login', to: 'auth#create'
+    get '/profile', to: 'users#profile'
+
+    post '/clients/csv', to: 'clients#csv'
+
+    get '/duedates', to: 'duedates#getAll'
+    post '/duedates/createMany', to: 'duedates#createMany'
+      
 end
