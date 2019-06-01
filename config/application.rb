0d7628a18a7f57df684d2800cbfb6e0cc2f36d75
Emily Wagner <EmilyWagner598@gmail.com>
2019-06-01 16:05:18 -0700

    for heroku

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,5 @@ module BackEndV001
     config.middleware.use ActionDispatch::Flash
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
-
-    config.api_only = true
   end
 end
